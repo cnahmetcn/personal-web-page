2d6d9d3f8b40c9e6036b6014af8ed40d33e2370b
Ahmet CAN <cnahmet@outlook.com>
2021-01-26 16:29:25 +0300

    finish

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Website.Models;
 
 namespace Website.Controllers
 {
@@ -11,6 +14,51 @@ namespace Website.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Mail m)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.gmail.com",587);
+                client.Credentials = new NetworkCredential("cnahmetcn@gmail.com", "mewglrmmmloobtfe");
+                client.EnableSsl = true;
+
+                MailMessage msj = new MailMessage();
+                msj.From = new MailAddress(m.Email, m.Name+" "+m.Surname);
+                msj.To.Add("cnahmetcn@gmail.com");
+                msj.Subject = m.Subject;
+                msj.Body = m.Message;
+
+                client.Send(msj);
+
+                MailMessage msj1 = new MailMessage();
+                msj1.From = new MailAddress("cnahmetcn@gmail.com","Ahmet Can");
+                msj1.To.Add(m.Email);
+                msj1.Subject = "Teşekkürler";
+                msj1.Body = "Benimle iletişime geçtiğiniz için teşekkür ederim";
+
+                client.Send(msj1);
+
+                ViewBag.Success = "Mailiniz alınmıştır. Teşekkür ederim";
+                return View();
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Mailiniz gönderilemedi. Lütfen tekrar deneyin";
+                return View();
+            }
         }
     }
 }
---
+++ Website/Models/Mail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Models
+{
+    public class Mail
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
